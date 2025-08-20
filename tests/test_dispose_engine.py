ae87866c764aabba744d2d84d99555d093cf3c5c
telix5000 <matthew.song@gmail.com>
2025-08-19 23:08:53 -0700

    Terminate pooled connections when disposing on closed loop

--- tests/test_dispose_engine.py
+++ tests/test_dispose_engine.py
@@ -6,12 +6,36 @@ from nzbidx_api import db
 
 
 def test_dispose_engine_handles_closed_loop(monkeypatch):
-    disposed = False
+    terminated = False
+
+    class DummyConn:
+        def terminate(self) -> None:
+            nonlocal terminated
+            terminated = True
+
+    class DummyRec:
+        def __init__(self) -> None:
+            self.dbapi_connection = DummyConn()
+
+    class DummyQueue:
+        def __init__(self) -> None:
+            self.items = [DummyRec()]
+
+        def get_nowait(self):
+            if self.items:
+                return self.items.pop()
+            raise Exception
+
+    class DummyPool:
+        def __init__(self) -> None:
+            self._pool = DummyQueue()
 
     class DummyEngine:
-        async def dispose(self):
-            nonlocal disposed
-            disposed = True
+        def __init__(self) -> None:
+            self.sync_engine = type("SyncEngine", (), {"pool": DummyPool()})()
+
+        async def dispose(self):  # pragma: no cover - should not be called
+            raise RuntimeError("dispose called")
 
     loop = asyncio.new_event_loop()
     loop.close()
@@ -21,6 +45,6 @@ def test_dispose_engine_handles_closed_loop(monkeypatch):
 
     asyncio.run(db.dispose_engine())
 
-    assert disposed
+    assert terminated
     assert db._engine is None
     assert db._engine_loop is None
